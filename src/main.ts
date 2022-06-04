874a4afdc60dbea21b82d0a29c2ed7d866fb4ae7
Pablowolf96 <ikhena.owen47@gmail.com>
2022-06-04 22:11:25 +0100

    heroku setup

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger();
-  const configService = new ConfigService();
-  const url = configService.get('DATABASE_URL');
-  logger.log(`Database URL: ${url}`);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  const PORT = process.env.TEST_PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
