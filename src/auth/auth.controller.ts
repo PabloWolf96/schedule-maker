37a69bbf6061847aaa5f3d5579328d786a527320
Pablowolf96 <ikhena.owen47@gmail.com>
2022-06-08 00:04:04 +0100

    serialize response, so that it excludes password and isActive

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
+import { UserDto } from '../users/dtos/user.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Serialize(UserDto)
   @Post('signup')
   createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     try {
