9b3d05bb4ca3defad3343929dddab928a6fd1aa9
Pablowolf96 <ikhena.owen47@gmail.com>
2022-06-15 08:37:12 +0100

    write unit tests for signupLocal method in authservice

--- src/auth/mocks/users-service.mock.ts
+++ src/auth/mocks/users-service.mock.ts
@@ -1,10 +1,10 @@
-import { userStub } from '../stubs/user.stub';
+import { newUserStub } from '../stubs/new-user.stub';
 
 export const UsersServiceMock = jest.fn().mockReturnValue({
-  findByEmail: jest.fn().mockResolvedValue(userStub()),
-  findByUsername: jest.fn().mockResolvedValue(userStub()),
-  add: jest.fn().mockResolvedValue(userStub()),
+  findByEmail: jest.fn().mockResolvedValue(newUserStub()),
+  findByUsername: jest.fn().mockResolvedValue(newUserStub()),
+  add: jest.fn().mockResolvedValue(newUserStub()),
   updateUser: jest.fn().mockResolvedValue(null),
-  findById: jest.fn().mockResolvedValue(userStub()),
+  findById: jest.fn().mockResolvedValue(newUserStub()),
   updateRefreshTokenHashToNull: jest.fn().mockResolvedValue(null),
 });
--- src/auth/tests/auth.service.spec.ts
+++ src/auth/tests/auth.service.spec.ts
@@ -1,12 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
+import { DuplicateEmailError } from '../../common/errors/errors';
+import { DuplicateUsernameError } from '../../common/errors/errors';
 import { UsersService } from '../../users/users.service';
 import { AuthService } from '../auth.service';
 import { ConfigServiceMock } from '../mocks/config-service.mock';
 import { JwtServiceMock } from '../mocks/jwt-service.mock';
 import { UsersServiceMock } from '../mocks/users-service.mock';
 import { createUserStub } from '../stubs';
+import { newUserStub } from '../stubs';
 import { UserDetails } from '../types';
 
 describe('AuthService', () => {
@@ -54,6 +57,53 @@ describe('AuthService', () => {
       it('should call findByUsername', () => {
         expect(usersService.findByUsername).toHaveBeenCalledWith(
           createUserStub().username,
+        );
+      });
+      it('should return an object with id, email, username, firstName, lastName, isActive, access_token, refresh_token', () => {
+        expect(response).toEqual({
+          id: expect.any(String),
+          email: expect.any(String),
+          username: expect.any(String),
+          firstName: expect.any(String),
+          lastName: expect.any(String),
+          isActive: expect.any(Boolean),
+          access_token: expect.any(String),
+          refresh_token: expect.any(String),
+        });
+      });
+    });
+    describe('when an email already exists', () => {
+      let spy: jest.SpyInstance;
+      beforeEach(async () => {
+        spy = jest
+          .spyOn(usersService, 'findByEmail')
+          .mockResolvedValue(newUserStub());
+      });
+      afterEach(() => {
+        spy.mockRestore();
+      });
+      it('it throws a duplicate email error', async () => {
+        await expect(authService.signupLocal(createUserStub())).rejects.toThrow(
+          DuplicateEmailError,
+        );
+      });
+    });
+    describe('when a username already exists', () => {
+      let spy1: jest.SpyInstance;
+      let spy2: jest.SpyInstance;
+      beforeEach(async () => {
+        spy1 = jest.spyOn(usersService, 'findByEmail').mockResolvedValue(null);
+        spy2 = jest
+          .spyOn(usersService, 'findByUsername')
+          .mockResolvedValue(newUserStub());
+      });
+      afterEach(() => {
+        spy1.mockRestore();
+        spy2.mockRestore();
+      });
+      it('throws a duplicate username error', async () => {
+        await expect(authService.signupLocal(createUserStub())).rejects.toThrow(
+          DuplicateUsernameError,
         );
       });
     });
