e9763a5359072e496b3f3dec0ab8d98d6fc7f7fd
Pablowolf96 <ikhena.owen47@gmail.com>
2022-06-15 11:01:44 +0100

    change refresh, access and hashed token in stubs

--- src/auth/mocks/jwt-service.mock.ts
+++ src/auth/mocks/jwt-service.mock.ts
@@ -1,10 +1,14 @@
 export class JwtServiceMock {
   signAsync(payload: any, options: any): Promise<string> {
     if (options.secret == 'JWT_SECRET') {
-      return Promise.resolve('access_token');
+      return Promise.resolve(
+        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkNGVkZDNlMi02OTg5LTQ3NjctYTNjOC0wMDJlMzBmZTM2YTYiLCJlbWFpbCI6ImlraGVuYS5vd2VuQGdtYWlsLmNvbSIsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoxNTE2MjQwMDAwfQ.TCglezB7r-IzZz5-rn5JiZ7IY2T3wXhkl25sJmUhE4E',
+      );
     }
     if (options.secret == 'JWT_SECRET_REFRESH') {
-      return Promise.resolve('refresh_token');
+      return Promise.resolve(
+        'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkNGVkZDNlMi02OTg5LTQ3NjctYTNjOC0wMDJlMzBmZTM2YTYiLCJlbWFpbCI6ImlraGVuYS5vd2VuQGdtYWlsLmNvbSIsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoxNTE2MjQwMDAwfQ.HfThMBsjyhPVqdEWyNIdW0m3l4f-I-3gDTpv4JHTRMQ',
+      );
     }
     return Promise.resolve('secret');
   }
--- src/auth/stubs/user.stub.ts
+++ src/auth/stubs/user.stub.ts
@@ -10,9 +10,12 @@ export const userStub = () => {
     lastName: 'Ikhena',
     createdAt: new Date(),
     updatedAt: new Date(),
-    hashedRT: null,
-    access_token: 'access_token',
-    refresh_token: 'refresh_token',
+    hashedRT:
+      '$argon2i$v=19$m=4096,t=3,p=1$RNg6IU+p1B3yC+8EgIps6Q$OqfSKYdoggrTS7i6XHk1GCT7lWsoBSu0RJykOWeFkZI',
+    access_token:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkNGVkZDNlMi02OTg5LTQ3NjctYTNjOC0wMDJlMzBmZTM2YTYiLCJlbWFpbCI6ImlraGVuYS5vd2VuQGdtYWlsLmNvbSIsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoxNTE2MjQwMDAwfQ.TCglezB7r-IzZz5-rn5JiZ7IY2T3wXhkl25sJmUhE4E',
+    refresh_token:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkNGVkZDNlMi02OTg5LTQ3NjctYTNjOC0wMDJlMzBmZTM2YTYiLCJlbWFpbCI6ImlraGVuYS5vd2VuQGdtYWlsLmNvbSIsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoxNTE2MjQwMDAwfQ.HfThMBsjyhPVqdEWyNIdW0m3l4f-I-3gDTpv4JHTRMQ',
     hashedPassword:
       '$argon2i$v=19$m=4096,t=3,p=1$sZvc3qoMohy8lLqUdBJLsg$6yjvUhIhl3VTKOldvWqmnLUkGLv6EY2YTx0RsTHm4Yc',
   };
