16cbc6da2ccc689c889f036caf25c9d9fc470a71
Pablowolf96 <ikhena.owen47@gmail.com>
2022-06-15 13:16:50 +0100

    write unit test for auth service

--- src/auth/tests/auth.service.spec.ts
+++ src/auth/tests/auth.service.spec.ts
@@ -8,13 +8,20 @@ import { AuthService } from '../auth.service';
 import { ConfigServiceMock } from '../mocks/config-service.mock';
 import { JwtServiceMock } from '../mocks/jwt-service.mock';
 import { UsersServiceMock } from '../mocks/users-service.mock';
-import { createUserStub } from '../stubs';
-import { newUserStub } from '../stubs';
+import {
+  createUserStub,
+  authStub,
+  newUserStub,
+  tokenStub,
+  userStub,
+} from '../stubs';
 import { UserDetails } from '../types';
 
 describe('AuthService', () => {
   let authService: AuthService;
   let usersService: UsersService;
+  let jwtService: JwtService;
+  let configService: ConfigService;
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [
@@ -29,6 +36,9 @@ describe('AuthService', () => {
     }).compile();
     authService = module.get<AuthService>(AuthService);
     usersService = module.get<UsersService>(UsersService);
+    jwtService = module.get<JwtService>(JwtService);
+    configService = module.get<ConfigService>(ConfigService);
+
     jest.clearAllMocks();
   });
   describe('signupLocal', () => {
@@ -105,6 +115,131 @@ describe('AuthService', () => {
         await expect(authService.signupLocal(createUserStub())).rejects.toThrow(
           DuplicateUsernameError,
         );
+      });
+    });
+  });
+  describe('signinLocal', () => {
+    describe('when it is called', () => {
+      let response;
+      let spy: jest.SpyInstance;
+      beforeEach(async () => {
+        spy = jest
+          .spyOn(usersService, 'findByEmail')
+          .mockResolvedValue(newUserStub());
+        response = await authService.signinLocal(authStub());
+      });
+      afterEach(() => {
+        spy.mockRestore();
+      });
+      it('calls usersService', () => {
+        expect(usersService.findByEmail).toHaveBeenCalledWith(authStub().email);
+      });
+      it('returns access_token and refresh_token', () => {
+        expect(response).toEqual(tokenStub());
+      });
+    });
+    describe('when it is called with an invalid password', () => {
+      it('returns null', async () => {
+        const response = await authService.signinLocal({
+          email: authStub().email,
+          password: 'WRONGpassword!23',
+        });
+        expect(response).toBeNull();
+      });
+    });
+    describe('when user does not exist', () => {
+      let spy: jest.SpyInstance;
+      beforeEach(() => {
+        spy = jest.spyOn(usersService, 'findByEmail').mockResolvedValue(null);
+      });
+      afterEach(() => {
+        spy.mockRestore();
+      });
+      it('should return null', async () => {
+        const response = await authService.signinLocal(authStub());
+        expect(response).toBeNull();
+      });
+    });
+  });
+  describe('logout', () => {
+    describe('when it is called', () => {
+      it('should call usersService', async () => {
+        await authService.logout(userStub().id);
+        expect(usersService.updateRefreshTokenHashToNull).toHaveBeenCalledWith(
+          userStub().id,
+        );
+      });
+    });
+  });
+  describe('refreshToken', () => {
+    describe('when it is called', () => {
+      let response;
+      const id = userStub().id;
+      const refreshToken = tokenStub().refresh_token;
+      beforeEach(async () => {
+        response = await authService.refreshTokens(id, refreshToken);
+      });
+      it('should call usersService', () => {
+        expect(usersService.findById).toHaveBeenCalledWith(id);
+      });
+      it('should return tokens', () => {
+        expect(response).toEqual(tokenStub());
+      });
+    });
+    describe('when it is called with an invalid refresh token', () => {
+      it('should return null', async () => {
+        const response = await authService.refreshTokens(
+          userStub().id,
+          'invalid-refresh-token',
+        );
+        expect(response).toBeNull();
+      });
+    });
+    describe('when the id is invalid', () => {
+      let spy;
+      const refreshToken = userStub().refresh_token;
+      beforeEach(() => {
+        spy = jest.spyOn(usersService, 'findById').mockResolvedValue(null);
+      });
+      afterEach(() => {
+        spy.mockRestore();
+      });
+      it('should return null', async () => {
+        const response = await authService.refreshTokens(
+          'invalid-id',
+          refreshToken,
+        );
+        expect(response).toBeNull();
+      });
+    });
+  });
+  describe('getTokens', () => {
+    describe('when it is called', () => {
+      let response;
+      beforeEach(async () => {
+        response = await authService['getTokens'](
+          userStub().id,
+          userStub().email,
+        );
+      });
+      it('should call jwt service twice', () => {
+        expect(jwtService.signAsync).toHaveBeenCalledTimes(2);
+      });
+      it('should call config service twice', () => {
+        expect(configService.get).toHaveBeenCalledTimes(2);
+      });
+      it('should return tokens', () => {
+        expect(response).toEqual(tokenStub());
+      });
+    });
+  });
+  describe('updateRefreshTokenHash', () => {
+    describe('when it is called', () => {
+      const id = userStub().id;
+      const refreshToken = userStub().refresh_token;
+      it('should call usersService', async () => {
+        await authService.updateRefreshTokenHash(id, refreshToken);
+        expect(usersService.updateUser).toHaveBeenCalled();
       });
     });
   });
